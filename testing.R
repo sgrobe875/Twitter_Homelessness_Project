91a8e3562398768b663dac293df87800272b33b2
sgrobe875 <sgrobe@uvm.edu>
2022-10-24 14:09:58 -0400

    added comments and documentation to functions

--- testing.R
+++ testing.R
@@ -1,6 +1,11 @@
+# import packages
 library(dplyr)
 library(ggplot2)
 
+
+
+# set working directory to the location of this file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
 # Recommended to run this line to clear environment, especially if cleaning.R was recently run:
@@ -10,10 +15,19 @@ rm(list = ls(all.names = TRUE))
 
 
 sent_barplot <- function() {
+  # create a dataframe with state and mean sentiment for that state
   state_sent <- geotagged %>% select(state, sentiment) %>% group_by(state) %>% mutate(mean_sent = mean(sentiment))
+  
+  # use stateNames to match the abbreviations to each state
   state_sent <- merge(state_sent, stateNames, by.x = "state", by.y = "abbrev")
-  state_sent <- state_sent %>% distinct(state, .keep_all = TRUE) %>% select(state, mean_sent) %>% arrange(mean_sent) 
+  
+  # remove duplicates so each state only appears once, and sort by mean sentiment (descending)
+  state_sent <- state_sent %>% distinct(state, .keep_all = TRUE) %>% select(state, mean_sent) %>% arrange(mean_sent)
+  
+  # convert state to a factor so the dataframe holds this ordering
   state_sent <- within(state_sent, state <- factor(state, levels = factor(state_sent$state)))
+  
+  # plot 
   ggplot(data = state_sent, mapping = aes(x = state, y = mean_sent)) + geom_col() +
     coord_flip() + 
     scale_y_continuous(expand = c(0, 0)) +
@@ -29,21 +43,31 @@ sent_barplot <- function() {
 
 
 sent_barplot_N <- function(N) {
+  # find the total number of tweets for each state
   state_sent <- merge(geotagged, stateNames, by.x = "state", by.y = "abbrev")
   state_sent <- state_sent %>% select(state) %>% group_by(state) %>% count 
+  
+  # create an additional dataframe of those being ignored (those with number of tweets < N)
   ignored <- state_sent %>% filter(n < N)
+  # filter the ignored out of the primary dataset as well
   state_sent <- state_sent %>% filter(n >= N)
+  
+  # print to the console how many we are ignoring
   print(paste(paste('Ignoring', nrow(ignored), 'due to low sample size', sep = ' ')))
   
-  # to avoid clutter, only print if 10 or fewer
+  # print the names of the states we are ignoring
+  # to avoid clutter, only print if 10 states or fewer
   if (nrow(ignored) < 11) {
     writeLines(ignored$state)
   }
   
+  # follow the same steps as in sent_barplot, but this time with the ignored states being removed
   temp <- geotagged %>% select(state, sentiment) %>% group_by(state) %>% mutate(mean_sent = mean(sentiment))
   state_sent <- merge(temp, state_sent, by.x = "state", by.y = "state")
   state_sent <- state_sent %>% distinct(state, .keep_all = TRUE) %>% select(state, mean_sent) %>% arrange(mean_sent) 
   state_sent <- within(state_sent, state <- factor(state, levels = factor(state_sent$state)))
+  
+  # plot
   ggplot(data = state_sent, mapping = aes(x = state, y = mean_sent)) + geom_col() +
     coord_flip() + 
     scale_y_continuous(expand = c(0, 0.001)) +
@@ -63,9 +87,10 @@ sent_barplot_N <- function(N) {
 
 
 
-# read in the cleaned data
+# read in the state names & abbreviations data
 source("stateNames.R")
-# geotagged <- read.csv('data/geotagged_sentiment_cleaned.csv')
+
+# read in the file that includes the sentiment analysis results
 geotagged <- read.csv('data/geotagged_sentiment_THIS_ONE.csv')
 
 
