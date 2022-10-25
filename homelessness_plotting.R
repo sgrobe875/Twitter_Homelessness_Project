a35113d59c95b2179cc150cee453dee1e4af79ba
sgrobe875 <sgrobe@uvm.edu>
2022-10-25 15:58:28 -0400

    investigating linear relationships

--- homelessness_plotting.R
+++ homelessness_plotting.R
@@ -65,6 +65,19 @@ ggplot(mapping = aes(x = log10(total_homeless_norm), y = log10(tweets_norm), col
   theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11))
 
 
+d %>% filter(mean_sent > 0) %>% 
+  ggplot(mapping = aes(x = log10(total_homeless_norm), y = log10(tweets_norm), color = mean_sent)) + 
+  geom_point(alpha = 0.8) + 
+  geom_smooth(method = 'lm', se = FALSE, color = 'black') +
+  sent_color_pallette + 
+  ggtitle('Volume of Tweets Per Capita by Total Homelessness Per Capita\nFor States with Mostly Positive Sentiment') +
+  xlab('Log10(Total Homelessness Per Capita)') + 
+  ylab('Log10(Number of Tweets Per Capita)') + 
+  labs(color = 'Average\nSentiment') + 
+  theme_dark() +
+  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11))
+
+
 d %>% filter(mean_sent < 0) %>% 
   ggplot(mapping = aes(x = log10(total_homeless_norm), y = log10(tweets_norm), color = mean_sent)) + 
   geom_point(alpha = 0.8) + 
@@ -75,6 +88,63 @@ d %>% filter(mean_sent < 0) %>%
   labs(color = 'Average\nSentiment') + 
   theme_dark() +
   theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11))
+
+
+d %>% filter(mean_sent < 0) %>% 
+  ggplot(mapping = aes(x = log10(total_homeless_norm), y = log10(tweets_norm), color = mean_sent)) + 
+  geom_point(alpha = 0.8) + 
+  geom_smooth(method = 'lm', se = FALSE, color = 'black') +
+  sent_color_pallette + 
+  ggtitle('Volume of Tweets Per Capita by Total Homelessness Per Capita\nFor States with Mostly Negative Sentiment') +
+  xlab('Log10(Total Homelessness Per Capita)') + 
+  ylab('Log10(Number of Tweets Per Capita)') + 
+  labs(color = 'Average\nSentiment') + 
+  theme_dark() +
+  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11))
+
+
+
+#### looking closer at the relationship for negative sentiment
+
+negative_sent <- d %>% filter(mean_sent < 0)
+negative_model <- lm(log10(negative_sent$tweets_norm) ~ log10(negative_sent$total_homeless_norm))
+
+# get intercept/slope
+negative_model$coefficients
+
+# correlation coefficient
+cor.test(log10(negative_sent$tweets_norm), log10(negative_sent$total_homeless_norm))
+
+
+
+#### same for positive sentiment because why not 
+positive_sent <- d %>% filter(mean_sent > 0)
+positive_model <- lm(log10(positive_sent$tweets_norm) ~ log10(positive_sent$total_homeless_norm))
+
+# get intercept/slope
+positive_model$coefficients
+
+# correlation coefficient
+cor.test(log10(positive_sent$tweets_norm), log10(positive_sent$total_homeless_norm))
+
+
+
+
+
+
+
+#### TODO: PLOT AVERAGE SENTIMENT BY HOMELESSNESS
+
+
+
+
+
+
+
+
+
+
+
 
 
 
