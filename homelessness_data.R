7a6dcbcf89c0fa3c3cbae467f9d0d6e61ab88bc1
sgrobe875 <sgrobe@uvm.edu>
2022-10-25 15:05:46 -0400

    completed joins and wrote final dataframe to csv file

--- homelessness_data.R
+++ homelessness_data.R
@@ -36,7 +36,7 @@ for (row in seq(1,nrow(tweet_counts))) {
   }
 }
 
-colnames(tweet_counts_long) <- c("state", "norm_count", "year") 
+colnames(tweet_counts_long) <- c("state", "tweets_norm", "year") 
 
 
 # join sentiment and tweet counts data by year & state
@@ -46,6 +46,65 @@ counts_and_sent <- merge(sent_state_year, tweet_counts_long, by = c('state', 'ye
 
 
 #### Repeat with homelessness counts ####
+
+# make one df for total homelessness and one for unsheltered homelessness
+total_homeless <- read.csv('data/normalized_total.csv')
+names(total_homeless) <- c('state','2010','2011','2012','2013','2014','2015','2016','2017')
+
+unsheltered_homeless <- read.csv('data/normalized_unsheltered.csv')
+names(unsheltered_homeless) <- c('state','2010','2011','2012','2013','2014','2015','2016','2017')
+
+
+
+
+
+# rework tweet counts to get in long format
+total_long <- data.frame(matrix(ncol = 3, nrow = 0))   # dataframe to hold long form data
+
+for (row in seq(1,nrow(total_homeless))) {
+  st <- total_homeless[row,'state']
+  for (col in seq(2,length(names(total_homeless)))) {
+    yr <- names(total_homeless)[col]
+    long_row <- c(st, total_homeless[row,col], yr)
+    total_long <- rbind(total_long, long_row)
+  }
+}
+
+colnames(total_long) <- c("state", "total_homeless_norm", "year") 
+
+
+
+
+
+
+# rework tweet counts to get in long format
+unshelt_long <- data.frame(matrix(ncol = 3, nrow = 0))   # dataframe to hold long form data
+
+for (row in seq(1,nrow(unsheltered_homeless))) {
+  st <- unsheltered_homeless[row,'state']
+  for (col in seq(2,length(names(unsheltered_homeless)))) {
+    yr <- names(unsheltered_homeless)[col]
+    long_row <- c(st, unsheltered_homeless[row,col], yr)
+    unshelt_long <- rbind(unshelt_long, long_row)
+  }
+}
+
+colnames(unshelt_long) <- c("state", "unshelt_homeless_norm", "year") 
+
+
+
+
+
+
+# join each of these to the prior one
+total_sent_counts <- merge(counts_and_sent, total_long, by = c('state', 'year'))
+
+everything <- merge(total_sent_counts, unshelt_long, by = c('state', 'year'))
+
+
+
+# write this final master dataframe to a file to save it
+write_csv(everything, 'data/homelessness_rate_data.csv')
 
 
 
