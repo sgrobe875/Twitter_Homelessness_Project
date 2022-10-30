23a4ad3098e8b0663b54df7b92d3337e12407701
sgrobe875 <sgrobe@uvm.edu>
2022-10-30 18:56:07 -0400

    adding grouping by month and by day

--- sentiment_analysis_wordfreqs.py
+++ sentiment_analysis_wordfreqs.py
@@ -294,16 +294,212 @@ def group_by_year(df):
 
 
 
+def get_months(date_time_list):
+    return_list = []
+    for string in date_time_list:
+        # split string into date and time
+        date_time_split = string.split('T')
+        # extract just the date
+        date = date_time_split[0]
+        # year-month = first 7 characters in string
+        return_list.append(date[0:7])
+        
+    return return_list
+
+
+def get_days(date_time_list):
+    return_list = []
+    for string in date_time_list:
+        # split string into date and time
+        date_time_split = string.split('T')
+        # extract just the date
+        date = date_time_split[0]
+        return_list.append(date)
+        
+    return return_list
+
+
+def group_by_month(df):
+    # begin by building the month column
+    print('Building month column')          
+            
+    df['month'] = get_months(list(df['created_at']))
+    
+    # group by month
+    print('Beginning grouping by month')
+    e = datetime.datetime.now()
+    print ("The current time is %s:%s:%s" % (e.hour, e.minute, e.second))
+    print()
+    
+    months = df['month'].unique()
+
+    sentiment_column = []
+    raw_sent_column = []
+    
+    # loop through all states
+    for m in months:
+        temp_tweets = df[df['month'] == m]
+            
+        # concatenate all these tweets into one large string
+        month_tweets = ''
+        for index in range(0, len(temp_tweets)):
+            month_tweets += temp_tweets.iloc[index]['text']
+            month_tweets += ' '  # add a space onto the end to avoid tweets merging together
+
+        del(temp_tweets)
+        
+        freqs = get_frequencies(month_tweets)
+        
+        del(month_tweets)
+        
+        this_group_sent = []
+        this_group_raw = []
+        
+        # need this line to avoid a ValueError in the for loop 
+        keys = list(freqs.keys())
+        
+        for key in keys:
+            raw_sent, vec = emotion(key, labMT, shift=True, happsList=labMTvector)
+            temp = stopper(vec,labMTvector,labMTwordList,stopVal=1.0)
+            sentiment = emotionV(temp,labMTvector)
+            
+            # negative sentiment means cannot be sentimentized, so let's remove it
+            if raw_sent < 0 or sentiment < 0:
+                freqs.pop(key)
+            
+            # if it was successfully sentimentized, append to the list so we can calc the avg
+            else:
+                this_group_sent.append(sentiment * freqs[key])
+                this_group_raw.append(raw_sent * freqs[key]) 
+
+        
+        # get the total number of words in the mega_sting (must be done after the for loop!)
+        num_words = sum(freqs.values())
+        
+        sentiment = sum(this_group_sent)/num_words
+        raw_sent = sum(this_group_raw)/num_words
+        
+        sentiment_column.append(sentiment)
+        raw_sent_column.append(raw_sent)
+        
+        print('Completed:', m)
+        e = datetime.datetime.now()
+        print ("The current time is %s:%s:%s" % (e.hour, e.minute, e.second))
+        print()
+    
+    # convert to dictionary and then to dataframe
+    month_grouped = {'month':months, 'sentiment':sentiment_column, 'raw_sent':raw_sent_column}
+    month_grouped = pd.DataFrame(month_grouped)
+
+    
+    # save the results to a file to move to R
+    month_grouped.to_csv('data/month_sentiment.csv', index = False)
+    
+    del(month_grouped)
+    
+    print('Completed sentiment analysis for grouping by month')
+    e = datetime.datetime.now()
+    print ("The current time is %s:%s:%s" % (e.hour, e.minute, e.second))
+
+
+
+def group_by_day(df):
+    # begin by building the day column
+    print('Building day column')
+            
+    df['day'] = get_days(list(df['created_at']))
+    
+    # group by day
+    print('Beginning grouping by day')
+    e = datetime.datetime.now()
+    print ("The current time is %s:%s:%s" % (e.hour, e.minute, e.second))
+    print()
+    
+    days = df['day'].unique()
+
+    sentiment_column = []
+    raw_sent_column = []
+    
+    # loop through all states
+    for d in days:
+        temp_tweets = df[df['day'] == d]
+            
+        # concatenate all these tweets into one large string
+        day_tweets = ''
+        for index in range(0, len(temp_tweets)):
+            day_tweets += temp_tweets.iloc[index]['text']
+            day_tweets += ' '  # add a space onto the end to avoid tweets merging together
+
+        del(temp_tweets)
+        
+        freqs = get_frequencies(day_tweets)
+        
+        del(day_tweets)
+        
+        this_group_sent = []
+        this_group_raw = []
+        
+        # need this line to avoid a ValueError in the for loop 
+        keys = list(freqs.keys())
+        
+        for key in keys:
+            raw_sent, vec = emotion(key, labMT, shift=True, happsList=labMTvector)
+            temp = stopper(vec,labMTvector,labMTwordList,stopVal=1.0)
+            sentiment = emotionV(temp,labMTvector)
+            
+            # negative sentiment means cannot be sentimentized, so let's remove it
+            if raw_sent < 0 or sentiment < 0:
+                freqs.pop(key)
+            
+            # if it was successfully sentimentized, append to the list so we can calc the avg
+            else:
+                this_group_sent.append(sentiment * freqs[key])
+                this_group_raw.append(raw_sent * freqs[key]) 
+
+        
+        # get the total number of words in the mega_sting (must be done after the for loop!)
+        num_words = sum(freqs.values())
+        
+        sentiment = sum(this_group_sent)/num_words
+        raw_sent = sum(this_group_raw)/num_words
+        
+        sentiment_column.append(sentiment)
+        raw_sent_column.append(raw_sent)
+        
+        print('Completed:', d)
+        e = datetime.datetime.now()
+        print ("The current time is %s:%s:%s" % (e.hour, e.minute, e.second))
+        print()
+    
+    # convert to dictionary and then to dataframe
+    day_grouped = {'day':days, 'sentiment':sentiment_column, 'raw_sent':raw_sent_column}
+    day_grouped = pd.DataFrame(day_grouped)
+
+    
+    # save the results to a file to move to R
+    day_grouped.to_csv('data/day_sentiment.csv', index = False)
+    
+    del(day_grouped)
+    
+    print('Completed sentiment analysis for grouping by day')
+    e = datetime.datetime.now()
+    print ("The current time is %s:%s:%s" % (e.hour, e.minute, e.second))
+
+
+
 ####################################
 
 
 
 df = pd.DataFrame(geotagged_tweets.loc[(geotagged_tweets["state"]!= "Puerto Rico") & (geotagged_tweets["state"] != "Unknown") & (geotagged_tweets['year'] != "")])
 
+group_by_month(df)
+group_by_day(df)
 
-group_by_both(df)
-group_by_year(df)
-group_by_state(df)
+
+# group_by_both(df)
+# group_by_year(df)
+# group_by_state(df)
 
 
 
