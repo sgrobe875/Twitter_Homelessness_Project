2ea4aa583de54efa95d1bd2c09f5c4f8c9e7724a
sgrobe875 <sgrobe@uvm.edu>
2022-10-30 23:05:58 -0400

    updated daily and monthly plotting with no params

--- timeseries_sentiment_graphing.R
+++ timeseries_sentiment_graphing.R
@@ -26,64 +26,13 @@ source('rescale_sentiment.R')
 
 # plot the sentiment over all years and states with daily averages
 sentiment_daily <- function(st = NULL, yr = NULL) {
-  
-  # group by date and find average sentiment
-  # sent_by_date <- geotagged %>% group_by(date_posted) %>% mutate(mean_sent = mean(sentiment)) %>% 
-  #   distinct(date_posted, .keep_all = TRUE)
-  
-  # set up plot title
-  title <- 'Daily Mean Sentiment Over Time'
-  
-  
-  ### filter the data according to function parameters ###
-  
-  # filtering by only year (st = year)
-  if (is.numeric(st)) {
-    sent_by_date <- geotagged %>% filter(year == as.character(st)) %>%  # filter out the year
-      group_by(date_posted) %>%                                         # group by month
-      mutate(mean_sent = mean(sentiment)) %>%                           # calculate mean for each group (month)
-      distinct(date_posted, .keep_all = TRUE) %>%                       # remove duplicates
-      select(date_posted, mean_sent, year, state)                       # extract useful columns (optional)
-    
-    title <- paste(title, ' (', as.character(st), ')', sep='')
-  }
-  
-  # no filtering (plotting all sentiment)
-  else if (is.null(st)) {
-    sent_by_date <- geotagged %>% group_by(date_posted) %>%    # no filtering needed, so just group by month
-      mutate(mean_sent = mean(sentiment)) %>%                  # calculate mean sentiment for each month
-      distinct(date_posted, .keep_all = TRUE) %>%              # remove duplicates
-      select(date_posted, mean_sent, year, state)              # extract useful columns (optional)
-  }
-  
-  # filtering by only state (st = state)
-  else if (is.null(yr)) {
-    sent_by_date <- geotagged %>% filter(state == st) %>%         # filter out the state
-      group_by(date_posted) %>%                                   # group by month
-      mutate(mean_sent = mean(sentiment)) %>%                     # calculate mean for each group (month)
-      distinct(date_posted, .keep_all = TRUE) %>%                 # remove duplicates
-      select(date_posted, mean_sent, year, state)                 # extract useful columns (optional)
-    
-    title <- paste(title, ' (', st, ')', sep='')
-  }
-  
-  # filtering by both (st = state, yr = year)
-  else {
-    sent_by_date <- geotagged %>% filter(state == st) %>%         # filter out the state
-      filter(year == yr) %>%                                      # filter out the year
-      group_by(date_posted) %>%                                   # group by month
-      mutate(mean_sent = mean(sentiment)) %>%                     # calculate mean for each group (month)
-      distinct(date_posted, .keep_all = TRUE) %>%                 # remove duplicates
-      select(date_posted, mean_sent, year, state)                 # extract useful columns (optional)
-    
-    title <- paste(title, ' (', st, ', ', as.character(yr), ')', sep='')
-  }
-
+  # convert strings to Dates
+  day_sent$day <- as.Date(day_sent$day)
   
   # plot
-  ggplot(data = sent_by_date, mapping = aes(x = date_posted, y = mean_sent)) + 
+  ggplot(data = day_sent, mapping = aes(x = day, y = sentiment)) + 
     geom_line() + 
-    ggtitle(title) + 
+    ggtitle('Daily Mean Sentiment Over Time') + 
     xlab('Time') + 
     ylab('Compound Sentiment') + 
     theme_bw() +
@@ -93,76 +42,19 @@ sentiment_daily <- function(st = NULL, yr = NULL) {
 
 # plot the sentiment over all years and states with monthly averages
 sentiment_monthly <- function(st = NULL, yr = NULL) {
-  # copy of main dataframe with only years and months
-  temp <- geotagged %>% mutate(month_posted = format_ISO8601(date_posted, precision = "ym"))
-  
-  # reformat so it plots at the first day of each month
-  temp$month_posted <- paste(temp$month_posted, '-01', sep = '')
-  temp$month_posted <- as.Date(temp$month_posted)
-  
-  # set up plot title
-  title <- 'Monthly Mean Sentiment Over Time'
-  
-  
-  
-  ### filter the data according to function parameters ###
-  
-  # filtering by only year (st = year)
-  if (is.numeric(st)) {
-    sent_by_date <- temp %>% filter(year == as.character(st)) %>%  # filter out the year
-      group_by(month_posted) %>%                                   # group by month
-      mutate(mean_sent = mean(sentiment)) %>%                      # calculate mean for each group (month)
-      distinct(month_posted, .keep_all = TRUE) %>%                 # remove duplicates
-      select(month_posted, mean_sent, year, state)                 # extract useful columns (optional)
-
-    title <- paste(title, ' (', as.character(st), ')', sep='')
-  }
-  
-  # no filtering (plotting all sentiment)
-  else if (is.null(st)) {
-    # st = 'dummy'
-    sent_by_date <- temp %>% group_by(month_posted) %>%    # no filtering needed, so just group by month
-      mutate(mean_sent = mean(sentiment)) %>%              # calculate mean sentiment for each month
-      distinct(month_posted, .keep_all = TRUE) %>%         # remove duplicates
-      select(month_posted, mean_sent, year, state)         # extract useful columns (optional)
-  }
-  
-  # filtering by only state (st = state)
-  else if (is.null(yr)) {
-    sent_by_date <- temp %>% filter(state == st) %>%               # filter out the state
-      group_by(month_posted) %>%                                   # group by month
-      mutate(mean_sent = mean(sentiment)) %>%                      # calculate mean for each group (month)
-      distinct(month_posted, .keep_all = TRUE) %>%                 # remove duplicates
-      select(month_posted, mean_sent, year, state)                 # extract useful columns (optional)
-    
-    # sent_by_date <- sent_by_date %>% filter(state == st)
-    title <- paste(title, ' (', st, ')', sep='')
-  }
-  
-  # filtering by both (st = state, yr = year)
-  else {
-    sent_by_date <- temp %>% filter(state == st) %>%               # filter out the state
-      filter(year == yr) %>%                                       # filter out the year
-      group_by(month_posted) %>%                                   # group by month
-      mutate(mean_sent = mean(sentiment)) %>%                      # calculate mean for each group (month)
-      distinct(month_posted, .keep_all = TRUE) %>%                 # remove duplicates
-      select(month_posted, mean_sent, year, state)                 # extract useful columns (optional)
-    
-    # sent_by_date <- sent_by_date %>% filter(state == st) %>% filter(year == as.character(yr))
-    title <- paste(title, ' (', st, ', ', as.character(yr), ')', sep='')
-  }
-  
-  
+  # convert strings to Dates
+  month_sent$month <- as.Date(paste(month_sent$month, '-01', sep=''))
   
   # plot
-  ggplot(data = sent_by_date, mapping = aes(x = month_posted, y = mean_sent)) + 
+  ggplot(data = month_sent, mapping = aes(x = month, y = sentiment)) + 
     geom_line() + 
-    ggtitle(title) + 
+    ggtitle('Monthly Mean Sentiment Over Time') + 
     xlab('Time') + 
     ylab('Compound Sentiment') + 
     theme_bw() +
     theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11))
 }
+  
 
 
 # plot the sentiment over all years and states with monthly averages
@@ -192,9 +84,9 @@ sentiment_yearly <- function() {
 ## Daily averages
 
 sentiment_daily()               # all sentiment
-sentiment_daily('CA')           # only California
-sentiment_daily(2017)           # only 2017
-sentiment_daily('CA', 2017)     # only California in 2017
+# sentiment_daily('CA')           # only California
+# sentiment_daily(2017)           # only 2017
+# sentiment_daily('CA', 2017)     # only California in 2017
 
 
 
@@ -202,12 +94,15 @@ sentiment_daily('CA', 2017)     # only California in 2017
 # Monthly averages
 
 sentiment_monthly()               # all sentiment
-sentiment_monthly('CA')           # only California
-sentiment_monthly(2017)           # only 2017
-sentiment_monthly('CA', 2017)     # only California in 2017
+# sentiment_monthly('CA')           # only California
+# sentiment_monthly(2017)           # only 2017
+# sentiment_monthly('CA', 2017)     # only California in 2017
 
 
 
+# Yearly averages
+
+sentiment_yearly()
 
 
 
