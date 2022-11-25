e6ce55bb62176f4a37544b8cc32da4f73f756020
sgrobe875 <sgrobe@uvm.edu>
2022-11-25 13:05:09 -0500

    save new plots to directory

--- eda.R
+++ eda.R
@@ -408,6 +408,146 @@ dev.off()
 
 
 
+
+
+
+#### changes in tweets vs changes in homelessness
+
+# build plots for the following states:
+states = c('CT','MA','GA','CA','FL','AR')
+
+# with bars plotted on top of each other:
+for (st in states) {
+  p <- ggplot(data = homeless_changes %>% filter(state == st) %>% within(year <- factor(year, levels = year))) +
+    geom_col(aes(x = year, y = total_homeless_change, fill = "Homelessness"),
+             alpha = 0.8) + 
+    geom_col(aes(x = year, y = tweet_norm_changes, group = 1, fill = "Tweets"),
+             alpha = 0.8) +
+    scale_fill_manual(values = c("darkblue","lightblue")) + 
+    labs(fill="") +
+    geom_hline(yintercept = 0) +
+    xlab('Year') + 
+    ylab('Change From Prior Year') + 
+    ggtitle(paste('Changes in Per Capita Tweets and Per Capita\nTotal Homelessness in', st)) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
+          axis.title.y = element_text(size = 9.5), legend.text = element_text(size = 11))
+  
+  print(p)
+  
+  f = paste("figures/eda/changes_homlessness_tweets_", st, ".png", sep = "")
+  png(filename=f, width=700, height=450)
+  print(p)
+  dev.off()
+}
+
+
+# with offset (grouped) bars:
+for (st in states) {
+  p <- ggplot(data = homeless_changes %>% filter(state == st) %>% within(year <- factor(year, levels = year))) +
+    geom_col(aes(x = year, y = total_homeless_change, fill = "Homelessness"),
+             width = 0.4, position = position_nudge(0.22)) + 
+    geom_col(aes(x = year, y = tweet_norm_changes, group = 1, fill = "Tweets"),
+             width = 0.4, position = position_nudge(-0.22)) +
+    scale_fill_manual(values = c("darkblue","lightblue")) + 
+    labs(fill="") +
+    geom_hline(yintercept = 0) +
+    xlab('Year') + 
+    ylab('Change From Prior Year') + 
+    ggtitle(paste('Changes in Per Capita Tweets and Per Capita\nTotal Homelessness in', st)) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
+          axis.title.y = element_text(size = 9.5))
+  
+  print(p)
+}
+
+
+
+
+
+
+
+
+
+#### Playing with the data ####
+
+
+
+# overlaying multiple distributions 
+# Note these don't have axis labels because the axes are kind of meaningless
+# the takeaway here is the shape of the distribution, not its values!
+p <- all_data %>% mutate(total_homeless_norm = total_homeless_norm / max(total_homeless_norm)) %>% 
+  mutate(tweets_norm = tweets_norm / max(tweets_norm)) %>% 
+  ggplot() +
+  geom_density(mapping = aes(x = log10(total_homeless_norm), fill = 'Homelessness'), color = 'black', alpha = 0.3) + 
+  geom_density(mapping = aes(x = log10(tweets_norm), fill = 'Tweets'), color = 'black', alpha = 0.3) + 
+  labs(fill="") +
+  scale_fill_manual(values = c("red","blue")) +
+  ggtitle('Distribution of Per Capita Homelessness and Per Capita Tweets,\nNormalized Between 0 and 1') + 
+  xlab('') + 
+  ylab('') +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
+        axis.title.y = element_text(size = 9.5))
+print(p)
+
+png(filename="figures/eda/compare_homelessness_tweets.png", width=700, height=450)
+p
+dev.off()
+
+
+
+
+
+# overlaid distributions for homelessness by year
+# The unicorn vomit plot!
+ggplot() + 
+  geom_density(data = all_data %>% filter(year == 2010), mapping = aes(x = total_homeless_norm,
+               fill = '2010'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2011), mapping = aes(x = total_homeless_norm,
+               fill = '2011'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2012), mapping = aes(x = total_homeless_norm,
+               fill = '2012'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2013), mapping = aes(x = total_homeless_norm,
+               fill = '2013'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2014), mapping = aes(x = total_homeless_norm,
+               fill = '2014'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2015), mapping = aes(x = total_homeless_norm,
+               fill = '2015'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2016), mapping = aes(x = total_homeless_norm,
+               fill = '2016'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2017), mapping = aes(x = total_homeless_norm,
+               fill = '2017'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2018), mapping = aes(x = total_homeless_norm,
+               fill = '2018'), color = 'black', alpha = 0.2) +
+  geom_density(data = all_data %>% filter(year == 2019), mapping = aes(x = total_homeless_norm,
+               fill = '2019'), color = 'black', alpha = 0.2) +
+  labs(fill="Year") +
+  scale_fill_manual(values = c('red','orange','yellow','green','blue','purple','gray','brown','magenta','cyan')) +
+  xlab('Per Capita Homelessness') + 
+  ylab('Density') + 
+  ggtitle('Distribution of Per Capita Total Homelessness By Year') +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
+        axis.title.y = element_text(size = 9.5))
+
+
+
+
+
+
+
+
+
+
+
+#### Some commented out scratchwork ####
+
+
+
+
+
 # normed tweet counts and homelessness counts per state
 # ggplot() + 
 #   geom_histogram(data = all_data %>% filter(state == 'CA'), mapping = aes(x = tweets_norm),
@@ -424,87 +564,18 @@ dev.off()
 
 
 
-ggplot(data = homeless_changes %>% filter(state == 'CA') %>% within(year <- factor(year, levels = year))) +
-  geom_col(mapping = aes(x = year, y = tweet_norm_changes),
-           fill = 'darkblue', color = 'darkblue', alpha = 0.8) +
-  geom_col(mapping = aes(x = year, y = total_homeless_change),
-               fill = 'lightblue', color = 'lightblue', alpha = 0.8) +
-  geom_hline(yintercept = 0) +
-  xlab('Year') + 
-  ylab('Change From Prior Year') + 
-  labs(color = 'screaming') +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
-        axis.title.y = element_text(size = 9.5))
-
-
-
-
-
-
-
-
-
-
-
-
-
-#### Playing with the data ####
-
-
-
-# overlaying multiple distributions - THIS IS UGLY AND INCOMPLETE  
-p <- all_data %>% mutate(total_homeless_norm = total_homeless_norm / max(total_homeless_norm)) %>% 
-  mutate(tweets_norm = tweets_norm / max(tweets_norm)) %>% 
-  ggplot() +
-  geom_density(mapping = aes(x = log10(total_homeless_norm)), color = 'black', fill = 'red', alpha = 0.3) + 
-  geom_density(mapping = aes(x = log10(tweets_norm)), color = 'black', fill = 'blue', alpha = 0.3) + 
-  ggtitle('Distribution of Log Tweet Counts per State/Year Pair') + 
-  xlab('Log10(Number of Tweets)') + 
-  ylab('Frequency') +
-  theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
-        axis.title.y = element_text(size = 9.5))
-print(p)
-
-
-
-
-
-
-# overlaid distributions for homelessness by year
-ggplot() + 
-  geom_density(data = all_data %>% filter(year == 2010), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'red', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2011), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'orange', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2012), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'yellow', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2013), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'green', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2014), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'blue', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2015), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'purple', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2016), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'gray', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2017), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'brown', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2018), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'magenta', alpha = 0.2) +
-  geom_density(data = all_data %>% filter(year == 2019), mapping = aes(x = total_homeless_norm),
-               color = 'black', fill = 'cyan', alpha = 0.2) +
-  theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
-        axis.title.y = element_text(size = 9.5))
-
-
-
-
-
-
-
-
+# ggplot(data = homeless_changes %>% filter(state == 'CA') %>% within(year <- factor(year, levels = year))) +
+#   geom_col(mapping = aes(x = year, y = tweet_norm_changes, fill = 'Per Capita Tweets'), alpha = 0.8) +
+#   geom_col(mapping = aes(x = year, y = total_homeless_change, fill = 'Per Capita Homeless'), alpha = 0.8) +
+#   geom_hline(yintercept = 0) +
+#   xlab('Year') + 
+#   ylab('Change From Prior Year') + 
+#   scale_color_manual(name='Legend', 
+#                      breaks=c('Per Capita Tweets', 'Per Capita Homeless'),
+#                      values=c('Per Capita Tweets'='darkblue', 'Per Capita Homeless'='lightblue')) +
+#   theme_bw() +
+#   theme(plot.title = element_text(hjust = 0.5), axis.text=element_text(size=11),
+#         axis.title.y = element_text(size = 9.5))
 
 
 
