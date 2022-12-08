3f588f5fa776e51d9987b846560ecd08475c6aa2
sgrobe875 <sgrobe@uvm.edu>
2022-12-08 10:49:42 -0500

    sets up file for conducting hypothesis tests

---
+++ hypothesis_testing.R
@@ -0,0 +1,17 @@
+# set working directory to the location of this file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+# Recommended but not required to clear environment
+rm(list = ls(all.names = TRUE)) 
+
+
+# use this R file to automatically read in the data and rescale the sentiment values
+source('rescale_sentiment.R')
+
+
+
+
+
+
+# create model
