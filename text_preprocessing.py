01f0727e6e57c7941168bf1280c45aac05d9c601
sgrobe875 <sgrobe@uvm.edu>
2022-12-31 14:44:21 -0500

    filters links, hashtags, certain punct on a sample string

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -14,42 +14,111 @@ import pandas as pd
 # geotagged_tweets = pd.read_csv('data/geotagged_cleaned.csv', dtype=str)
 
 sample = "TeSTINg tweet so i can @username make sUre thi's work\"s! what? @another_username else. can} i& put* here$"
+sample += ' str8 gtfo bby https://google.com/testing/this_is_a_url/ you &amp; I '
+sample += '#Homeless #HashtagCheck #ImTryingMyBest'
+
+
+sample = 'I\'m trying...my best. Right-now. right/left' 
+
+
+
 
 # list of acceptable characters (what we DON'T want filtered out)
+letters =    ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+              'n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+letters_upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
+                 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+digits =     ['0','1','2','3','4','5','6','7','8','9']
+
 acceptable = ['a','b','c','d','e','f','g','h','i','j','k','l','m',   # letters
               'n','o','p','q','r','s','t','u','v','w','x','y','z',
               '0','1','2','3','4','5','6','7','8','9',               # numbers
               ' ']                                                   # space
+
+# slang dictionary! Used to enhance the sentiment analysis
+# all abbreviations included here are NOT already in the labMT dictionary, but
+# all words in the translation ARE! (or are symbols, and will be filtered out)
+slang = {'foh': 'fuck outta here', 'gtfo': 'get the fuck out', 'str8': 'straight',
+         'bby': 'baby', 'hon': 'hun', '&amp;': 'and', '&lt;': '<', '&gt;': '>'}
 
 
 #### TEXT PREPROCESSING ######################################################
 
 # loop through every tweet in the data set
 
+# split hashtags by camelcase
+new_tweet = sample
+# search for pound symbol
+while new_tweet.rfind('#') > -1:
+    # isolate the hashtag
+    hash_start = new_tweet.rfind('#')
+    hash_end = hash_start + 1
+    while new_tweet[hash_end] != ' ' and hash_end < len(new_tweet)-1:
+        hash_end += 1
+        
+    fixed_hash = new_tweet[hash_start+1:hash_end+1]
+    
+    for i in range(1, len(fixed_hash)-1):
+        # if uppercase letter
+        if fixed_hash[i] in letters_upper:
+            before = fixed_hash[i-1]
+            after = fixed_hash[i+1]
+            # if lowercase on either side of the uppercase letter
+            if before in letters and after in letters:
+                # add in a space before the capital
+                fixed_hash = fixed_hash[:i] + ' ' + fixed_hash[i:]
+                
+    # replace the fixed hash in new_tweet
+    new_tweet = new_tweet[:hash_start] + fixed_hash + new_tweet[hash_end+1:]
+                    
+
 #  set all characters in tweet to lowercase
-new_tweet = sample.lower()
+new_tweet = new_tweet.lower()
 
 # first filter out usernames (any substring starting with @ and ending in a space)
 while new_tweet.rfind('@') > -1:
     username_start = new_tweet.rfind('@')
     i = 1
-    username_end = username_start + i
+    username_end = username_start + 1
     # find the end of the username
-    while  new_tweet[username_end] != ' ' and username_end < len(sample):
-        i += 1
-        username_end = username_start + i
+    while  new_tweet[username_end] != ' ' and username_end < len(new_tweet):
+        username_end += 1
     # remove the username
     new_tweet = new_tweet[:username_start] + new_tweet[username_end+1:]
         
+# links! filter out anything with the substring http (so this includes https!)
+while new_tweet.rfind('http') > -1:
+    start = new_tweet.rfind('http')
+    end = start + 1
+    while end < len(new_tweet) and new_tweet[end] != ' ':
+        end += 1
+        
+    new_tweet = new_tweet[:start] + new_tweet[end+1:]
+    
+# loop through and search for items in the slang dictionary, replace as needed
+for key in slang.keys():
+    while new_tweet.rfind(key) > -1:
+        start = new_tweet.rfind(key)   
+        end = start + len(key)
+        new_tweet = new_tweet[:start] + slang[key] + new_tweet[end:]
+    
 # loop through every character in the tweet
 filtered_tweet = ''
+prev = ''
 for char in new_tweet:
     # if character is a-z, 0-9, or a space, append it to filtered_tweet (otherwise ignore)
     if char in acceptable:
+        # note that this causes some excess spaces, but fixes issue from ellipses, hyphens, etc.
+        if (prev == '.' and char != ' ') or prev == '-' or prev == '/':
+            filtered_tweet += ' '
         filtered_tweet += char
-
-    # after looping through all characters, overwrite the tweet with the 
-    # contents of filtered_tweet
+    prev = char
+        
+        
+# after looping through all characters, overwrite the tweet with the 
+# contents of filtered_tweet
 
 # write the preprocessed data set to a file
 
@@ -75,10 +144,10 @@ print(filtered_tweet)
 
 # Filter out retweets
 
-# start by searching for the substring "rt:"
+# start by searching for the substring "rt @"
 
-# if the above is the first three characters of a tweet, delete that tweet
-# from the data set
+# save only the substring that comes before this (if it's a QRT, there will be
+# content, but if not I think it will just be empty?)
 
 # also loop through and check for any identical tweets in the data set:
     
